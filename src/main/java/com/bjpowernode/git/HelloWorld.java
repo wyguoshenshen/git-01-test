83039d29d73d89a8786f61f071ba266f5c50418b
guoshen <18401297531@163.com>
2020-02-14 20:22:03 +0800

    init git-01-test

---
+++ src/main/java/com/bjpowernode/git/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * @项目名：git-01-test
+ * @创建人： gss
+ * @创建时间： 2020-02-14
+ * @公司： www.bjpowernode.com
+ * @描述：TODO
+ */
+package com.bjpowernode.git;
+
+/**
+ * <p>NAME: HelloWorld</p>
+ * @author gss
+ * @date 2020-02-14 19:34:49
+ * @version 1.0
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
